3a2a3ee975b90a23c6e5504ca57ab40626648bf1
Yue-plus <Yue_plus@foxmail.com>
2021-08-13 15:15:42 +0800

    update 404 page

--- packages/vuepress-theme-oxygen-not-included/index.ts
+++ packages/vuepress-theme-oxygen-not-included/index.ts
@@ -5,7 +5,7 @@ module.exports = {
   extends: '@vuepress/theme-default',
   layouts: {
     Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
-    // 404: path.resolve(__dirname, 'layouts/404.vue'),
+    404: path.resolve(__dirname, 'layouts/404.vue'),
   },
   plugins: [
     [
